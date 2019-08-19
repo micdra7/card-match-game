33fd8f180e684b0af945022ec45cb960cd111a94
Michał Drabik <micdra7@gmail.com>
2019-08-19 19:29:01 +0200

    Cards now adjusted with CSS's Grid

--- src/components/CardCollection/CardCollection.js
+++ src/components/CardCollection/CardCollection.js
@@ -14,14 +14,23 @@ const CardCollection = ({ cards, matchedCards, difficulty, set, select, initiali
     const renderedContent = cards.length === 0 ? <ScoreboardInput /> : cards.map((card, index) => {
         const cardClassName = matchedCards.includes(card) ? 'card invisible' : 'card';
         const handleClick = matchedCards.includes(card) ? () => {} : () => select(card.x, card.y);
+        const cardStyle = {width: `${100/(difficulty*2)}%`, height: `${100/(difficulty*2)}%`};
 
         return (
-            <Card key={card.value * index + set} value={card.value} set={set} handleClick={handleClick} className={cardClassName} />
+            <Card style={cardStyle} key={card.value * index + set} value={card.value} set={set} handleClick={handleClick} className={cardClassName} />
         );
     });
 
+    const gridForDifficulty = ['50% 50%', '25% 25% 25% 25%', 
+        '16.67% 16.67% 16.67% 16.67% 16.67% 16.67%', '12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5%']
+
+    const style = {
+        gridTemplateColumns: gridForDifficulty[difficulty-1],
+        gridTemplateRows: gridForDifficulty[difficulty-1]
+    };
+
     return (
-        <div className="card-collection">
+        <div className="card-collection" style={style}>
             {renderedContent}
         </div>
     );
